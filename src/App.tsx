ddd302efa5270458d956d6e8fd46060ec72f444c
majiang <majiang@users.noreply.github.com>
2021-03-16 11:11:50 +0900

    directive node is replaced by its content(s) without handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useRemark } from 'react-remark'
+import remarkDirective from 'remark-directive'
+
+import './App.css'
+
+function App()
+{
+    const [reactContent, setMarkdownSource] = useRemark(
+    {
+        remarkPlugins:
+        [
+            remarkDirective,
+        ],
+    })
+    useEffect(() =>
+    {
+        setMarkdownSource(
+        [
+            '# simple (not nested) directives',
+            ':test[inline content]{attr=val1}',
+            '::test[leaf content]{attr=val2}',
+            ':::test[inline-content]{attr=val3}',
+            'content',
+            ':::',
+            '',
+            '# nested directives',
+            ':test[inline *em* content]{attr=val4}',
+            '::test[leaf *strong* content]{attr=val5}',
+            ':::test[inline-`code`-content]{attr=val6}',
+            'content',
+            ':::',
+            ''
+        ].join('\n'))
+    }, [])
+    return reactContent || <></>
 }
 
-export default App;
+export default App
